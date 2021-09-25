9a9249ff0a3c9c0469aebcf625c9b7c13f7869bb
SpencerDiamond <sdiamond@cheyenne3.cheyenne.ucar.edu>
2021-09-25 17:25:11 -0600

    added sleep files

--- cheyenneSingleNode/dataScripts/resetData.py
+++ cheyenneSingleNode/dataScripts/resetData.py
@@ -1,4 +1,4 @@
-files = ["waccmturboData.csv", "waccmratedData.csv", "waccmslowData.csv", "mg2ratedData.csv", "mg2turboData.csv", "mg2slowData.csv", "clubbturboData.csv", "clubbratedData.csv", "clubbslowData.csv"]
+files = ["waccmturboData.csv", "waccmratedData.csv", "waccmslowData.csv", "mg2ratedData.csv", "mg2turboData.csv", "mg2slowData.csv", "clubbturboData.csv", "clubbratedData.csv", "clubbslowData.csv","sleepslowData.csv","sleepratedData.csv","sleepturboData.csv"]
 
 for f in files:
 	s=open(f, 'w')
