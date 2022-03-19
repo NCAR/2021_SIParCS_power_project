1730f4f670b07cdfee7129eef837db16eac2f7e6
SpencerDiamond <sdiamond@cheyenne5.cheyenne.ucar.edu>
2022-03-19 13:42:44 -0600

    checkData.py now checks for power reading failure

--- cheyenneSingleNode/dataScripts/checkData.py
+++ cheyenneSingleNode/dataScripts/checkData.py
@@ -6,13 +6,19 @@ m.close()
 
 for jobID in IDlist:
 	jobFile = jobID+".OU"
-	
 	check = []
+	sum=0
+	
 	f=open(jobFile, 'r')
 	for line in f:
 		check.append(line)
 	f.close()
 
-	if len(check) < 5:
+	if len(check) < 10:
 		print(jobFile, check[0])
 
+	for i in range(3,len(check)-1):
+		sum += int(check[i])
+
+	if sum == 0:
+		print(jobFile, check[0])
