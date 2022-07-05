63c1e42113b2d1f3f3ca3425da7424fe3056d32f
SpencerDiamond <sdiamond@cheyenne4.cheyenne.ucar.edu>
2022-07-05 12:42:50 -0600

    added output

--- dataScripts/fixtime.py
+++ dataScripts/fixtime.py
@@ -1,3 +1,4 @@
+#!/bin/ env python
 import datetime
 import pytz
 from pytz import timezone
@@ -10,13 +11,10 @@ dataFile = sys.argv[1]
 csvfile = open(dataFile)
 data = np.loadtxt(csvfile, delimiter=",", dtype="int")
 
-print(data)
-
-newData = np.zeros((len(data),2), dtype="int")
+fout=open(dataFile, 'w')
 for j in range(0, len(data)):
     dt = mount.localize(datetime.datetime(data[j][6],data[j][1],data[j][2],data[j][3],data[j][4],data[j][5]))
     utcdt = dt.astimezone(pytz.utc)
     timestamp = int(utcdt.timestamp())
-    newData[j] = [data[j][0], timestamp]
-
-print(newData)
+    fout.write(str(data[j][0])+','+str(timestamp)+'\n')
+fout.close()
