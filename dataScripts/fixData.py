fce36ec547f67bf8b0bc5159aab2e911bba64c19
SpencerDiamond <86630451+SpencerDiamond@users.noreply.github.com>
2022-07-18 17:53:56 -0600

    implimented interpolation

--- dataScripts/fixData.py
+++ dataScripts/fixData.py
@@ -21,8 +21,14 @@ for j in range(0, len(data)):
     power.append(data[j][0])
 
 spl = interpolate.splrep(times,power)
-for t in range(times[0],times[len(times)-1]):
-    print(t)
+check = 0
+while check == 0:
+    check = 1
+    for t in range(1,len(times)-1):
+        if (times[t]-1) != times[t-1]:
+            times.insert(t,times[t-1]+1)
+            power.insert(t,interpolate.splev(times[t-1]+1, spl))
+            check = 0
 
 fout=open(dataFile, 'w')
 for k in range(0, len(power)):
