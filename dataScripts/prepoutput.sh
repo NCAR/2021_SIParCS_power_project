f47ea55a66466fb128d5c9cad8da4cace60b9a37
SpencerDiamond <86630451+SpencerDiamond@users.noreply.github.com>
2022-07-26 12:33:08 -0600

    removed extra variables

--- dataScripts/prepoutput.sh
+++ dataScripts/prepoutput.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 jobnode=$(echo $1 | sed 's/^[a-z]*//')
-job=$(echo $jobnode | cut -d "_" -f1)
-node=$(echo $jobnode | cut -d "_" -f2)
 
 sed -i '/I/!d' $jobnode #delete lines without 'Instant' or 'IMPI timestamp'
 sed -i 's/Jan/1/g;s/Feb/2/g;s/Mar/3/g;s/Apr/4/g;s/May/5/g;s/Jun/6/g;s/Jul/7/g;s/Aug/8/g;s/Sep/9/g;s/Oct/10/g;s/Nov/11/g;s/Dec/12/g' $jobnode #replace month name with number
