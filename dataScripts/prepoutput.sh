7372f5b11c751e4518068eb8839fea3351299439
SpencerDiamond <86630451+SpencerDiamond@users.noreply.github.com>
2022-08-01 15:34:34 -0600

    fixed doubled space oversight

--- dataScripts/prepoutput.sh
+++ dataScripts/prepoutput.sh
@@ -7,4 +7,4 @@ sed -i 's/Jan/1/g;s/Feb/2/g;s/Mar/3/g;s/Apr/4/g;s/May/5/g;s/Jun/6/g;s/Jul/7/g;s/
 sed -i 's/^[A-Za-z \t:]*//;s/[A-Za-z \t:]*$//' $jobnode #trim unwanted characters
 paste -sd ',\n' $jobnode >> temp$jobnode #replace newlines with commas, combining reading with time
 mv temp$jobnode $jobnode
-sed -i 's/[ :]/,/g' $jobnode #change to full csv format
+sed -i 's/  /,/g;s/[ :]/,/g' $jobnode #change to full csv format
