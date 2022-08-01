541903819be9ca02f46578e80de095827630f58c
SpencerDiamond <86630451+SpencerDiamond@users.noreply.github.com>
2022-08-01 16:15:11 -0600

    added .csv suffix to output files after formatting

--- dataScripts/cleanData.sh
+++ dataScripts/cleanData.sh
@@ -2,4 +2,4 @@
 
 #conda activate npl
 
-for file in `ls | grep $1`; do ./prepoutput.sh $file; python3 fixData.py $file; done
+for file in `ls | grep $1`; do ./prepoutput.sh $file; python3 fixData.py $file.csv; done
