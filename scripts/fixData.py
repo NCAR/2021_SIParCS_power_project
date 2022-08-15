0bc175b842c602c85700feed55762115bcc422d7
SpencerDiamond <86630451+SpencerDiamond@users.noreply.github.com>
2022-08-15 14:37:51 -0600

    added reading error protection

--- scripts/fixData.py
+++ scripts/fixData.py
@@ -12,16 +12,29 @@ mount = timezone('US/Mountain')
 dataFile = sys.argv[1]
 jID = dataFile.split("_")[0]
 jNode = dataFile.split("_")[1].split(".")[0]
-csvfile = open(dataFile)
-data = np.loadtxt(csvfile, delimiter=",", dtype="int")
+csvFile = open(dataFile)
+data = np.loadtxt(csvFile, delimiter=",", dtype="int")
 
 power = []
 times = []
-for j in range(0, len(data)):
+for j in range(len(data)):
     dt = mount.localize(datetime.datetime(data[j][6],data[j][1],data[j][2],data[j][3],data[j][4],data[j][5]))
     utcdt = dt.astimezone(pytz.utc)
     times.append(int(utcdt.timestamp()))
     power.append(data[j][0])
+
+cTimes = times.copy()
+m = 0
+count = 0
+fault = 0
+for n in range(len(cTimes)-1):
+    if cTimes[n] == cTimes[n+1]:
+        fault = cTimes[n]
+        count = cTimes.count(fault)
+        for r in range(count-1):
+            times.pop(n-m)
+            power.pop(n-m)
+        m += count
 
 totJ = integrate.simps(power,times)
 totT = times[len(times)-1] - times[0]
@@ -30,17 +43,20 @@ spl = interpolate.splrep(times,power)
 check = 0
 while check == 0:
     check = 1
-    for t in range(0,len(times)-1):
+    for t in range(len(times)-1):
         if (times[t]+1) != times[t+1]:
             times.insert(t+1,times[t]+1)
             power.insert(t+1,interpolate.splev(times[t]+1, spl))
             check = 0
 
 fout=open(dataFile, 'w')
-for k in range(0, len(power)):
+for k in range(len(power)):
     fout.write(str(times[k])+','+str(round(float(power[k])))+'\n')
 fout.close()
 
-gout=open(jID + "Data.csv", 'a')
-gout.write(jNode +','+ str(totT) +','+ str(round(totJ)) +'\n')
-gout.close()
+if totJ == 0:
+    print("dcmi power reading failure: "+ dataFile.split(".")[0])
+else:
+    gout=open(jID + "Data.csv", 'a')
+    gout.write(jNode +','+ str(totT) +','+ str(round(totJ)) +'\n')
+    gout.close()
