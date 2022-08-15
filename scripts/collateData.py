3a27dc869c4db6a47b0065eb8e5df8106b693d16
SpencerDiamond <86630451+SpencerDiamond@users.noreply.github.com>
2022-08-15 14:53:58 -0600

    changed viable name

--- scripts/collateData.py
+++ scripts/collateData.py
@@ -13,17 +13,17 @@ for line in m:
 m.close()
 
 jName = data.pop(0)
-tTot = 0
+tMax = 0
 pSum = 0
 n = 0
 
 for k in data:
     newT = int(k.split(",")[1])
-    if newT > tTot:
-        tTot = newT
+    if newT > tMax:
+        tMax = newT
     pSum += int(k.split(",")[2])
     n += 1
 
 f=open("Data.csv", 'a')
-f.write(jName +','+ str(tTot) +','+ str(pSum) +'\n')
+f.write(jName +','+ str(tMax) +','+ str(pSum) +'\n')
 f.close()
