8b9f01eb6689d077ade0e4772f2fd88df2eee64c
SpencerDiamond <86630451+SpencerDiamond@users.noreply.github.com>
2022-08-19 14:14:36 -0600

    fixed path name

--- scripts/powerDataCollect.sh
+++ scripts/powerDataCollect.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-hostid=$(hostname)
-datadir=$(echo $PWD | sed 's%scripts%data%')
-while true; do sudo ipmitool dcmi power reading >> $datadir/${1}_$hostid; sleep 1; done 
+while true; do sudo ipmitool dcmi power reading >> ${1}_$(hostname); sleep 1; done 
